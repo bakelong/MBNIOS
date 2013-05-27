a46d162db55fb27999c3e967bc29b356ee025c2a
Nguyễn văn long <bakelong1837461998@yahoo.com>
2013-05-27 14:17:04 +0700

    create mnnios

---
+++ js/common.js
@@ -0,0 +1,14 @@
+﻿
+
+function checkLogin(){
+	var username= localStorage.getItem("username");
+	if(username==null || username=='')
+		return false;
+	return true;
+}
+
+function writeHeader()
+{
+
+
+}
---
+++ js/mainMenu.js
@@ -0,0 +1,23 @@
+﻿	var onSettings = function() {
+        console.log("settings");
+    };
+ 
+    var onHelp = function() {
+        console.log("help");
+    };
+ 
+    var optionsmenu = new OptionsMenu({
+        id: "optionsmenu",
+        items: [ 
+            [ {
+                label: "Settings",
+                image: "menu/settings.png",
+                action: onSettings
+            }, 
+            {
+                label: "Help",
+                image: "menu/help.png",
+                action: onHelp
+            } ]
+        ]
+    });
